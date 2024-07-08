4a4e67e4e979d135a0e801465f83bcde05506e62
Bharat Kasera <bharat.codewolf@gmail.com>
2024-07-08 13:55:34 +0530

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ const Home = ({ searchParams }: SearchParamProps) => {
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 CarePluse
+              © 2024 DocFlow
             </p>
             <Link href="/?admin=true" className="text-green-500">
               Admin
--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -73,7 +73,7 @@ const RequestSuccess = async ({
           </Link>
         </Button>
 
-        <p className="copyright">© 2024 CarePluse</p>
+        <p className="copyright">© 2024 DocFlow</p>
       </div>
     </div>
   );
